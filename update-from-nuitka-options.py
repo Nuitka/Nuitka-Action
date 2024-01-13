c45f6a8991e5357e942634b2c1eae28251428929
Kay Hayen <kay.hayen@gmail.com>
2024-01-13 16:25:16 +0100

    Added script to import options from existing Nuitka and add it via Jinja2 template mechanism.

---
+++ update-from-nuitka-options.py
@@ -0,0 +1,66 @@
+import os
+import textwrap
+
+from nuitka.OptionParsing import SUPPRESS_HELP, parser
+from nuitka.utils.CommandLineOptions import OurOptionParser
+from nuitka.utils.FileOperations import changeTextFileContents
+from nuitka.utils.Jinja2 import getTemplate
+
+template = getTemplate(
+    package_name=None,
+    template_subdir=os.path.dirname(__file__) or ".",
+    template_name="action.yml.j2",
+    extensions=("jinja2.ext.do",),
+)
+
+
+def getTopOptions():
+    for option in parser.iterateOptions():
+        # Help option
+        if "--help" in option._long_opts:
+            continue
+
+        # Main option is currently not done like this
+        if "--main" in option._long_opts:
+            continue
+
+        if not hasattr(option, "require_compiling"):
+            continue
+
+        # Non-compiling options do not belong into Nuitka-Action
+        if not option.require_compiling or not option.github_action:
+            continue
+
+        if option.help is SUPPRESS_HELP:
+            continue
+
+        container = getattr(option, "container", None)
+
+        if isinstance(container, OurOptionParser):
+            yield option
+
+
+def formatOption(option):
+    return (
+        option._long_opts[0].lstrip("-")
+        + ":\n  description: |\n"
+        + textwrap.indent(option.help, prefix="    ")
+    )
+
+
+def get_top_options():
+    result = []
+
+    for option in getTopOptions():
+        result.append(formatOption(option))
+
+    # assert False, "\n".join(result)
+    return textwrap.indent("\n".join(result), "  ")
+
+
+action_yaml = template.render(get_top_options=get_top_options)
+
+if changeTextFileContents("action.yml", action_yaml):
+    print("Updated.")
+else:
+    print("Already up to date.")
